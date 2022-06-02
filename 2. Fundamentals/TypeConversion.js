a4a330408c1fa1d52a7e22708cc2ce75c02697eb
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2022-06-02 22:56:38 +0530

    Day 4: Type Conversions

---
+++ 2. Fundamentals/TypeConversion.js
@@ -0,0 +1,10 @@
+// let a = "VISHI";
+
+// console.log(Number(a));
+
+// console.log(typeof  Number());
+
+// console.log(Number("            d  123 "));
+
+let myEmptyObj = [];
+console.log(Boolean(myEmptyObj));
