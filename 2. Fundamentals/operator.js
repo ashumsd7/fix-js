de9d05ea1d33b9bbe56f5a5d91d54d017f8ccbea
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2022-06-03 23:08:56 +0530

    Day 5: Operators

---
+++ 2. Fundamentals/operator.js
@@ -0,0 +1,94 @@
+
+
+
+// MATH OPERATOR
+
+
+// console.log(1**3);
+// console.log(6**3);
+
+
+
+// console.log(15 % 0);
+
+// root
+
+
+
+// cube root
+
+// console.log(12**(1/3));
+
+
+// // square root
+
+
+
+// console.log(144 ** (1 / 2));
+
+
+
+
+// How to convert in a number
+
+
+
+// let a = '5'
+
+
+// console.log(typeof a);
+
+
+
+
+// console.log(Number(a));
+// console.log(typeof Number(a));
+
+// let b="6"
+
+// console.log(typeof b);
+
+// b= +b
+
+// console.log(typeof b);
+
+
+
+// let x = 5;
+
+
+
+// // x = x * 2
+
+
+// x*=2
+
+
+// console.log(x);
+
+let vidhiKiAge = 45;
+
+console.log(vidhiKiAge--);
+
+console.log(vidhiKiAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
